27a05ef8d4e55c8656ada0586310014c20d7ca08
Vinci4600 <vincent.diergardt@gmail.com>
2026-06-30 12:01:10 +0200

    Add Sqlite in MainActivity

--- app/src/main/java/com/example/einkaufsliste/MainActivity.java
+++ app/src/main/java/com/example/einkaufsliste/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.einkaufsliste;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-import android.content.SharedPreferences;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -14,9 +14,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DBHelper dbHelper;
     private ArrayList<String> produktListe = new ArrayList<>();
-    private static final String PREFS_NAME = "EinkaufslistePrefs";
-    private static final String KEY_LISTE = "produktListe";
     private TextView textViewListe;
 
     private ActivityResultLauncher<Intent> addProductLauncher;
@@ -26,12 +25,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dbHelper = new DBHelper(this);
+
         Button button = findViewById(R.id.addbtn);
         textViewListe = findViewById(R.id.textViewListe);
-        loadListe();
-        updateListe();
 
-        // RESULT HANDLING
+        //  Launcher zuerst registrieren
+        setupLauncher();
+
+        //  Daten laden
+        ladeProdukte();
+
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddActivity.class);
+            addProductLauncher.launch(intent);
+        });
+    }
+
+    //  RESULT HANDLING
+    private void setupLauncher() {
+
         addProductLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 result -> {
@@ -39,73 +52,49 @@ public class MainActivity extends AppCompatActivity {
                     if (result.getResultCode() == RESULT_OK && result.getData() != null) {
 
                         Intent data = result.getData();
-// Get Data
+
                         String produkt = data.getStringExtra("produkt");
                         String beschreibung = data.getStringExtra("beschreibung");
                         String menge = data.getStringExtra("menge");
                         String preis = data.getStringExtra("preis");
 
-                        String eintrag = "- " + produkt + " | " +
-                                beschreibung + " | " +
-                                menge + " | CHF " + preis;
-// Produkt Eintagen
-                        produktListe.add(eintrag);
-// Und dann liste aktualiesieren
-                        saveListe();
-                        updateListe();
+                        dbHelper.insertProdukt(produkt, beschreibung, menge, preis);
 
+                        ladeProdukte();
                     }
                 }
         );
-
-// Button Velinkung zu AddActivity
-        button.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, AddActivity.class);
-            addProductLauncher.launch(intent);
-        });
     }
-    //funktion Save List
-    private void saveListe() {
-// öffnet speicher bereich
-        SharedPreferences prefs =
-                getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-// edit wird geöffnet
-        SharedPreferences.Editor editor = prefs.edit();
-// langer string wird gebaut
-        StringBuilder sb = new StringBuilder();
-// Item wird in die Produkt Liste gesetzt ;;; = attributet z.B. Milch ,Zucker, Mehl
-        for (String item : produktListe) {
-            sb.append(item).append(";;;");
+
+    private void ladeProdukte() {
+
+        produktListe.clear();
+
+        Cursor cursor = dbHelper.getProdukte();
+
+        while (cursor.moveToNext()) {
+
+            String produkt = cursor.getString(1);
+            String beschreibung = cursor.getString(2);
+            String menge = cursor.getString(3);
+            String preis = cursor.getString(4);
+
+            String eintrag =
+                    "- " + produkt +
+                            " | " + beschreibung +
+                            " | " + menge +
+                            " | CHF " + preis;
+
+            produktListe.add(eintrag);
         }
-//speichert den string unter Key Liste
-        editor.putString(KEY_LISTE, sb.toString());
-        editor.apply();
-    }
-    // Funktion Liste Laden
-    private void loadListe() {
-//öffnet Speicher bereich der app
-        SharedPreferences prefs =
-                getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-// Greift auf die gespeicherte Key Liste zu
-        String gespeicherteListe =
-                prefs.getString(KEY_LISTE, "");
-// falls liste leer
-        if (!gespeicherteListe.isEmpty()) {
-//teile ;;; in drei einzelne items also MilchZuckerMehl = Milch, Zucker und Mehl
-            String[] items = gespeicherteListe.split(";;;");
 
-            produktListe.clear();
+        cursor.close();
 
-            for (String item : items) {
-
-                if (!item.isEmpty()) {
-                    produktListe.add(item);
-                }
-            }
-        }
+        updateListe();
     }
 
     private void updateListe() {
+
         StringBuilder sb = new StringBuilder();
 
         for (String item : produktListe) {
